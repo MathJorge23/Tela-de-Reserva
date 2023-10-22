80e33983cfa245b12886965184e9afb227b7212b
Matheus <mathjor.silva@gmail.com>
2023-10-22 13:48:52 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardapioComponent } from './cardapio/cardapio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardapioComponent
   ],
   imports: [
     BrowserModule,
